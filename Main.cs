c4e20bd3f106dc3fbe687dc7236c8a0d66ac4aac
Akune122 <loiczaercher2@gmail.com>
2024-03-19 15:06:26 +0100

    pushpush

--- Main.cs
+++ Main.cs
@@ -8,6 +8,8 @@ class Program {
 
     Console.WriteLine("Merci de saisir votre pseudo de joueur !");
     string nom_j1=Console.ReadLine();
+    Console.WriteLine("Appuyer sur une touche commencer la partie !");
+    Console.ReadLine();
 
     Joueur j1;
     j1 = new Joueur(nom_j1,"*",0);
@@ -72,26 +74,56 @@ class Program {
     Legume co4=null;
     Viande co5=null;
     Pain co6=null;
+//-------------------------------
+//Critique HISTOIRES
+//-------------------------------
 
+string histoire_claire="  Claire BASTIEN est une pilote de Formule 1 connue dans le monde entier !\n 133 victoires en grand prix, + de 200 podiums et + de 100 poles positions.\n Elle a révelé son secret après son 8ème sacre de championne du monde.\n Elle mange énormément de cheddar dans des burgers et adore visiter les plus grands restaurants du monde.\n En revanche, elle est allergique aux cepe...";
 
 //-------------------------------
 // Clients
 //-------------------------------
 
-    Client c1;
-    c1 = new Client("ZAERCHER","Lucas","<Bonjour, je veux un burger simple ! Mais je n'aime que le steak et pas les oignons -_- >",0,oignon,steak);
+Client c1;
+c1 = new Client("ZAERCHER","Lucas","<Bonjour, je veux un burger simple ! Mais je n'aime que le steak et pas les oignons -_- >",0,oignon,steak);
+
+Client c2;
+c2 = new Client("HERBAULT","Mathieu","<Bonjour, je voudrai un burger avec du rumsteak mais surtout pas de cheddar je suis gourmet.On est pas au mcdo. >",0,cheddar,rumsteak);
+
+Client c3;
+c3 = new Client("ZAERCHER","Olga","<Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! (Vous croyez entendre poulet et tomate)>",0,tomate,poulet);
+
+Client c4;
+c4 = new Client("ZAERCHER","Yves","<Bonjour mon petit gars, j'aimerai un burger avec de la salade absolument mais je n'aime pas l'emmental !>",0,emmental,salade);
+
+Critique critique1;
+critique1 = new Critique(histoire_claire,"31/07/2005","BASTIEN","Claire","<Bonjour, j'aimerai manger aujourd'hui.>",20,oignon,cheddar);
+
+//-------------------------------
+// Listes Client
+//-------------------------------
+
+Client[] rep0 = new Client[] {c1,c2,c3,c4};
+Critique[] rep20 = new Critique[] {critique1};
 
 
-
+//-------------------------------
+// Code
+//-------------------------------
     do {
+    foreach (var client in rep0) {
+        if (j1.getRep()>19 || j1.getRep()<0)
+    {
+        continue;
+    }
     Console.WriteLine("----------------------------------------------------------------------------------------");
     Console.WriteLine("Votre pseudo : "+j1.getPseudo()+" / Votre réputation : "+j1.getRep()+" / Vos étoiles : "+j1.getEtoiles());
     Console.WriteLine("----------------------------------------------------------------------------------------");
 
     Console.WriteLine("Appuyer sur une touche pour recevoir un client...");
     Console.ReadLine();
-    Console.WriteLine("Voici votre premier client :"+c1.getPrenom()+" "+c1.getNom());
-    Console.WriteLine(c1.getDialogue());
+    Console.WriteLine("Voici votre premier client :"+client.getPrenom()+" "+client.getNom());
+    Console.WriteLine(client.getDialogue());
 
     Console.WriteLine("");
 
@@ -210,7 +242,6 @@ class Program {
     verif=false;
 
     do {
-
     Console.WriteLine("");
     Console.WriteLine("Choisir votre cinquième couche :");
     Console.WriteLine("1.Steak / 2.Poulet / 3.Bacon / 4.Rumsteak");
@@ -291,29 +322,278 @@ class Program {
     Console.WriteLine("Appuyer sur une touche pour continuer...");
     Console.ReadLine();
     
-    if (c1.verifB1(bj_1)==true && c1.verifB2(bj_1)==false) 
+    if (client.verifB1(bj_1)==true && client.verifB2(bj_1)==false) 
     {
-        Console.WriteLine(c1.getPrenom()+" : Merci le burger était parfait !");
+        Console.WriteLine(client.getPrenom()+" : Merci le burger était parfait !");
         j1.updateRep(10);
     }
-    else if (c1.verifB1(bj_1)==false && c1.verifB2(bj_1)==false)
+    else if (client.verifB1(bj_1)==false && client.verifB2(bj_1)==false)
     {
-        Console.WriteLine(c1.getPrenom()+" : Je trouve qu'il manquait quelque chose...");
+        Console.WriteLine(client.getPrenom()+" : Je trouve qu'il manquait quelque chose...");
         j1.updateRep(5);
     }
-    else if (c1.verifB1(bj_1)==true && c1.verifB2(bj_1)==true)
+    else if (client.verifB1(bj_1)==true && client.verifB2(bj_1)==true)
     {
-        Console.WriteLine(c1.getPrenom()+" : Y'a un truc que je peux pas manger là-dedans");
+        Console.WriteLine(client.getPrenom()+" : Y'a un truc que je peux pas manger là-dedans !");
         j1.updateRep(-5);
     }
     else {
-        Console.WriteLine(c1.getPrenom()+" : HORRIBLE !");
+        Console.WriteLine(client.getPrenom()+" : HORRIBLE !");
         j1.updateRep(-10);
     }
 
-    Console.WriteLine(" ");
+    Console.WriteLine("");
+    Console.WriteLine("Continuer à jouer...");
+    Console.ReadLine();
+    }
 
-    }while(j1.getRep()>=0);
+    }while(j1.getRep()>=0 && j1.getRep()<20);
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//Suite du code (reputation>20)
+//-------------------------------------------------------------------------------------------------------------------
+    do {
+    foreach (var client in rep20) {
+        if (j1.getRep()>50 || j1.getRep()<20)
+    {
+        continue;
+    }
+    Console.WriteLine("----------------------------------------------------------------------------------------");
+    Console.WriteLine("Votre pseudo : "+j1.getPseudo()+" / Votre réputation : "+j1.getRep()+" / Vos étoiles : "+j1.getEtoiles());
+    Console.WriteLine("----------------------------------------------------------------------------------------");
+
+    Console.WriteLine("Appuyer sur une touche pour recevoir un client...");
+    Console.ReadLine();
+    Console.WriteLine("Voici votre premier client :"+client.getPrenom()+" "+client.getNom());
+    Console.WriteLine(client.getDialogue());
+    Console.WriteLine("Voici l'histoire de "+client.getPrenom());
+    Console.WriteLine(client.getHistoire());
+
+    Console.WriteLine("");
+
+    do {
+    Console.WriteLine("Choisir votre première couche :");
+    Console.WriteLine("1.Campagne / 2.Sésame / 3.Nature / 4.Mie");
+    choix = Console.ReadLine();
+    if (choix==campagne.getAffichage())
+    {       
+        co1=campagne;
+        verif=true;
+    }
+    else if (choix==sesame.getAffichage())
+    {
+        co1=sesame;
+        verif=true;
+    }
+    else if (choix==mie.getAffichage())
+    {
+        co1=mie;
+        verif=true;
+    }
+    else if (choix==nature.getAffichage())
+    {
+        co1=nature;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+
+    verif=false;
+
+    do {
+    Console.WriteLine("");
+    Console.WriteLine("Choisir votre deuxième couche :");
+    Console.WriteLine("1.Ketchup / 2.Mayo / 3.Poivre / 4.Moutarde");
+    choix = Console.ReadLine();
+    if (choix==ketchup.getAffichage())
+    {       
+        co2=ketchup;
+        verif=true;
+    }
+    else if (choix==mayo.getAffichage())
+    {
+        co2=mayo;
+        verif=true;
+    }
+    else if (choix==poivre.getAffichage())
+    {
+        co2=poivre;
+        verif=true;
+    }
+    else if (choix==moutarde.getAffichage())
+    {
+        co2=moutarde;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+    verif=false;
+
+    do {
+    Console.WriteLine("");
+    Console.WriteLine("Choisir votre troisième couche :");
+    Console.WriteLine("1.Cheddar / 2.Emmental / 3.Parmesan");
+    choix = Console.ReadLine();
+    if (choix==cheddar.getAffichage())
+    {       
+        co3=cheddar;
+        verif=true;
+    }
+    else if (choix==emmental.getAffichage())
+    {
+        co3=emmental;
+        verif=true;
+    }
+    else if (choix==parmesan.getAffichage())
+    {
+        co3=parmesan;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+    verif=false;
+
+    do {
+    Console.WriteLine("");
+    Console.WriteLine("Choisir votre quatrième couche :");
+    Console.WriteLine("1.Salade / 2.Tomate / 3.Oignon");
+    choix = Console.ReadLine();
+    if (choix==salade.getAffichage())
+    {       
+        co4=salade;
+        verif=true;
+    }
+    else if (choix==tomate.getAffichage())
+    {
+        co4=tomate;
+        verif=true;
+    }
+    else if (choix==oignon.getAffichage())
+    {
+        co4=oignon;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+    verif=false;
+
+    do {
+    Console.WriteLine("");
+    Console.WriteLine("Choisir votre cinquième couche :");
+    Console.WriteLine("1.Steak / 2.Poulet / 3.Bacon / 4.Rumsteak");
+    choix = Console.ReadLine();
+    if (choix==steak.getAffichage())
+    {       
+        co5=steak;
+        verif=true;
+    }
+    else if (choix==poulet.getAffichage())
+    {
+        co5=poulet;
+        verif=true;
+    }
+    else if (choix==bacon.getAffichage())
+    {
+        co5=bacon;
+        verif=true;
+    }
+    else if (choix==rumsteak.getAffichage())
+    {
+        co5=rumsteak;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+    verif=false;
+
+    do {
+
+    Console.WriteLine("");
+    Console.WriteLine("Choisir votre dernière couche :");
+    Console.WriteLine("1.Campagne / 2.Sésame / 3.Nature / 4.Mie");
+    choix = Console.ReadLine();
+    if (choix==campagne.getAffichage())
+    {       
+        co6=campagne;
+        verif=true;
+    }
+    else if (choix==sesame.getAffichage())
+    {
+        co6=sesame;
+        verif=true;
+    }
+    else if (choix==mie.getAffichage())
+    {
+        co6=mie;
+        verif=true;
+    }
+    else if (choix==nature.getAffichage())
+    {
+        co6=nature;
+        verif=true;
+    }
+    else {
+        Console.WriteLine("Choix invalide...");
+    }
+    }while(verif!=true);
+
+    verif=false;
+
+    Burger bj_1;
+    bj_1 = new Burger(co1,co2,co3,co4,co5,co6);
+
+    Console.WriteLine("");
+    Console.WriteLine("Votre burger est constitué de :");
+    Console.WriteLine(bj_1.getCouche(1));
+    Console.WriteLine(bj_1.getCouche(2));
+    Console.WriteLine(bj_1.getCouche(3));
+    Console.WriteLine(bj_1.getCouche(4));
+    Console.WriteLine(bj_1.getCouche(5));
+    Console.WriteLine(bj_1.getCouche(6));
+
+    Console.WriteLine(" ");
+    Console.WriteLine("Vérification du burger :");
+    Console.WriteLine("Appuyer sur une touche pour continuer...");
+    Console.ReadLine();
+    
+    if (client.verifB1(bj_1)==true && client.verifB2(bj_1)==false) 
+    {
+        Console.WriteLine(client.getPrenom()+" : Merci le burger était parfait !");
+        j1.updateRep(25);
+    }
+    else if (client.verifB1(bj_1)==false && client.verifB2(bj_1)==false)
+    {
+        Console.WriteLine(client.getPrenom()+" : Je trouve qu'il manquait quelque chose...");
+        j1.updateRep(10);
+    }
+    else if (client.verifB1(bj_1)==true && client.verifB2(bj_1)==true)
+    {
+        Console.WriteLine(client.getPrenom()+" : Y'a un truc que je peux pas manger là-dedans !");
+        j1.updateRep(-10);
+    }
+    else {
+        Console.WriteLine(client.getPrenom()+" : HORRIBLE !");
+        j1.updateRep(-15);
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("Continuer à jouer...");
+    Console.ReadLine();
+    }
+
+    }while(j1.getRep()>=20 && j1.getRep()<50);
 
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 C:\Users\loicz\Documents\GitHub\cookingburger
 
 Burger(Pain couche1,Sauce couche2,Fromage couche3,Legume couche4,Viande couche5,Pain couche6)
+
+
+Clients : 
+
+
+---------------------------------------------------------------------------------------------------------------------------
+Client c1;
+c1 = new Client("ZAERCHER","Lucas","<Bonjour, je veux un burger simple ! Mais je n'aime que le steak et pas les oignons -_- >",0,oignon,steak);
+
+Client c2;
+c2 = new Client("HERBAULT","Mathieu","<Bonjour, je voudrai un burger avec du rumsteak mais surtout pas de cheddar je suis gourmet.On est pas au mcdo. >",0,cheddar,rumsteak);
+
+Client c3;
+c3 = new Client("ZAERCHER","Olga","<Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! Miaou miaou ! (Vous croyez entendre poulet et tomate)>",0,tomate,poulet);
+
+Client c4;
+c4 = new Client("ZAERCHER","Yves","<Bonjour mon petit gars, j'aimerai un burger avec de la salade absolument mais je n'aime pas l'emmental !>",0,emmental,salade);
+
+---------------------------------------------------------------------------------------------------------------------------
+
+Critique 
+
+Critique critique1;
+critique1 = new Critique("BASTIEN","Claire","<Bonjour, j'aimerai manger aujourd'hui.>",20,oignon,cheddar,histoire_claire,"31/07/2005");
+
